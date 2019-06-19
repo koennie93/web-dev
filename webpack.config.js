d661923c4d5c4efffded7bb3cd986986a0ef8527
koenvanrijn21 <koenvanrijn21@gmail.com>
2019-06-19 20:37:26 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(
+            {
+                cleanOnceBeforeBuildPatterns: [ '!.git' ]
+            }),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+     })
+   ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../wamp64/www')
+    },
+       module: {
+         rules: [
+             {
+                 test: /\.css$/,
+                 use: [
+                   'style-loader',
+                   'css-loader'
+                 ]
+             },
+             {
+                 test: /\.(png|svg|jpg|gif)$/,
+                 use: [
+                    'file-loader'
+                 ]
+             }
+         ]
+       }
+};
